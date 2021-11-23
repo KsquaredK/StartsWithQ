2b780098a2698520c9267e3ecbd178f30d59212f
KsquaredK <ksquaredkelly@gmail.com>
2021-11-23 14:38:07 -0500

    build search for 'ends with'

--- src/components/findWord/FindWordSearch.js
+++ src/components/findWord/FindWordSearch.js
@@ -7,6 +7,7 @@ import logo from "../auth/Qlogo.png";
 
 /* ======= PSEUDOCODE = MORE FEATURES ======= 
 Users can search for words that have Q, X, or Z in them (exploiting what code can do that our brains can’t),
+*In development* search for words that end with q, x, or z. (see below and FindWordProvider),
 Users can search for words by number of letters and position in word where the tricky letter occurs. For example, 
 they can search for an 8-letter word with an X in the 5th position to discover the word ‘relaxant”. Or just 
 search for 5-letter words with Q in them.
@@ -21,7 +22,7 @@ since it’s the most used authority in word games. */
 
 
 export const FindWordSearch = () => {
-    const {searchLetters, getSearchLetters, getWords, chosenLetter, setChosenLetter} = useContext(FindWordContext);
+    const {searchLetters, getSearchLetters, getWords, getWordsByLastLetter, chosenLetter, setChosenLetter} = useContext(FindWordContext);
 
     
     useEffect(() => {
@@ -57,68 +58,101 @@ export const FindWordSearch = () => {
       };
     }
 
+    // ====== Write New Function: handleSearchWordsByLastLetter  
+          // this function runs when user hits submit button. if no dropdown option was chosen, show window alert
+      const handleSearchWordsByLastLetter = (event) => {
+        if (
+          chosenLetter === "") {
+          window.alert("Please select a letter");
+        } else {
+          // disable the button - no extra clicks
+        // setIsLoading(true);
+        if (chosenLetter) {
+            //GET list of words and redirect rendered view to FindWordList
+          getWordsByLastLetter(chosenLetter)
+          .then(() => history.push(`/words/list`));
+        }
+        console.log(chosenLetter)
+      };
+    } 
+
     return (
     
       <>
-        <header>     
+      <header>
           <div className="header">
-            <img src={logo} alt="Starts With Q logo"/>
+            <img src={logo} alt="Starts With Q logo" />
           </div>
-        </header>
-        <form className="findWordForm">
+      </header>
+      <form className="findWordForm">
           <h6 className="search-descrip">Find tricky words that use Q, X or Z</h6>
           <fieldset>
-              <div className="form-group">
-                  <select
-                      name="searchLetter"
-                      id="searchLetterId"
-                      className="form-control"
-                      value={searchLetters.id}
-                      key="searchLetterId"
-                      required
-                      onChange={handleControlledInputChange}>
-                      <option className="dropdown-label" value="0"> first choose a letter</option>
-                      {searchLetters.map((s) => (
-                      <option className="dropdown-label" key={s.id} value={s.name}>
-                        {s.name}
-                      </option>
-                    ))}
-                  </select>
-                  <p></p>  
+            <div className="form-group">
+              <select
+                name="searchLetter"
+                id="searchLetterId"
+                className="form-control"
+                value={searchLetters.id}
+                key="searchLetterId"
+                required
+                onChange={handleControlledInputChange}>
+                <option className="dropdown-label" value="0"> first choose a letter</option>
+                {searchLetters.map((s) => (
+                  <option className="dropdown-label" key={s.id} value={s.name}>
+                    {s.name}
+                  </option>
+                ))}
+              </select>
+              <p></p>
 
-              </div>
-{/* Search for words that end with q/x/z: refactor functions, add search to FindWordPRovider */}
-              {/* <div className="form-group">
-                  <select
-                      name="searchLetter"
-                      id="searchLetterId"
-                      className="form-control"
-                      value={searchLetters.id}
-                      key="searchEndLetterId"
-                      required
-                      onChange={handleControlledInputChange}>
-                      <option className="dropdown-label" value="0"> Or choose the letter your words end with</option>
-                      {searchLetters.map((s) => (
-                      <option className="dropdown-label" key={s.id} value={s.name}>
-                        {s.name}
-                      </option>
-                    ))}
-                  </select>
-                  <p></p>  
-              </div> */}
+            </div>
 
-          </fieldset>  
-          
-            <Button
-                className="btn"
-                color="secondary"
-                onClick={(event) => {
-                //Prevents the browser from submitting the form
-                event.preventDefault();
-                handleSearchWordsByFirstLetter()}}>
-                  search</Button>{''}
+          </fieldset>
+        </form>
+
+          <Button
+            className="btn" color="secondary" onClick={(event) => {
+              //Prevents the browser from submitting the form
+              event.preventDefault();
+              handleSearchWordsByFirstLetter();
+            }}>search</Button>{''}
+
+          {/*</form> ===== Search for words that end with q/x/z: refactor functions, add search to FindWordPRovider */}
+
+        <form> 
+
+          <fieldset>
+            <div className="form-group">
+              <select
+                name="searchLetter"
+                id="searchLetterId"
+                className="form-control"
+                value={searchLetters.id}
+                key="searchEndLetterId"
+                required
+                onChange={handleControlledInputChange}>
+                <option className="dropdown-label" value="0"> Or choose the letter your words end with</option>
+                {searchLetters.map((s) => (
+                  <option className="dropdown-label" key={s.id} value={s.name}>
+                    {s.name}
+                  </option>
+                ))}
+              </select>
+              <p></p>
+            </div> 
+
+          <Button
+            className="btn"
+            color="secondary"
+            onClick={(event) => {
+              //Prevents the browser from submitting the form
+              event.preventDefault();
+              handleSearchWordsByLastLetter();
+            }}>
+              search</Button>{''}
+        </fieldset>
              
-    </form>
+      </form>
     </>
     );}
 
